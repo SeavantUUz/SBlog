0bb13ec4d4bc4fa5d4fc069a717737f26dec811a
AprocySanae <axdiaoqi220@gmail.com>
2013-05-19 21:24:24 +0000

    delete my password...

--- blog/settings.py
+++ blog/settings.py
@@ -14,8 +14,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'blog_db',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': 'root',
-        'PASSWORD': 'zllgcr1123',
+        '': '',
+        '': '',
         'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
     }
