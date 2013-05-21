8970dc5bb34014d65d835299a16c663695db4931
AprocySanae <axdiaoqi220@gmail.com>
2013-05-21 22:19:54 +0000

    delete password...

--- blog/settings.py
+++ blog/settings.py
@@ -15,7 +15,7 @@ DATABASES = {
         'NAME': 'blog_db',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': 'root',
-        'PASSWORD': 'zllgcr1123',
+        'PASSWORD': '',
         'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
     }
